0ec1f7d018adda1c95d9c1288231f0dc1448490e
Kirill <K.zyuz@yandex.ru>
2026-01-31 15:37:52 +0300

    fix: icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,33 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/icon'
+  ],
   css: ['~/assets/css/main.css'],
-  ui: {
-    fonts: false
+  app: {
+    baseURL: '/diplom/'
   },
-
   icon: {
+    provider: 'none',
     clientBundle: {
-      scan: true,
+      scan: {
+        globInclude: [
+          'components/**/*.vue',
+          'app.vue',
+          'pages/**/*.vue',
+          'app/**/*.vue',
+          'node_modules/@nuxt/ui/dist/**'
+        ],
+        globExclude: ['node_modules']
+      },
       icons: [
         'heroicons-outline:chevron-down',
         'heroicons-outline:chevron-up',
         'heroicons-outline:trash',
         'heroicons-outline:pencil',
         'heroicons-outline:calendar',
-      ]
+      ],
+      sizeLimitKb: 512
     }
-  },
-  app: {
-    baseURL: '/diplom/'
   }
 })
