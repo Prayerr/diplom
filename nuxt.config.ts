2c2d695dc6e675ec30151226df4fcb98a78c03d0
Kirill <K.zyuz@yandex.ru>
2026-01-31 15:40:48 +0300

    fix: icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,26 +8,12 @@ export default defineNuxtConfig({
     baseURL: '/diplom/'
   },
   icon: {
-    provider: 'none',
-    clientBundle: {
-      scan: {
-        globInclude: [
-          'components/**/*.vue',
-          'app.vue',
-          'pages/**/*.vue',
-          'app/**/*.vue',
-          'node_modules/@nuxt/ui/dist/**'
-        ],
-        globExclude: ['node_modules']
-      },
-      icons: [
-        'heroicons-outline:chevron-down',
-        'heroicons-outline:chevron-up',
-        'heroicons-outline:trash',
-        'heroicons-outline:pencil',
-        'heroicons-outline:calendar',
-      ],
-      sizeLimitKb: 512
-    }
+    provider: 'iconify',
+    serverBundle: false, 
+    clientBundle: { 
+      icons: [],      
+      scan: false    
+    },
+    fallbackToApi: true 
   }
 })
